d97ec5846ec7c535463f1b5a2a9b9dd7d25853bc
CarlosAllen93 <allencarlosdev@gmail.com>
2021-08-24 21:22:28 -0400

    adding vendor publish livewire

---
+++ resources/views/livewire/navigation.blade.php
@@ -0,0 +1,10 @@
+<header class="bg-Emerald-900">
+    <div class="container flex items-center h-16">
+        <a class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full" href="http://">
+            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+            <span class="">Categories</span>
+        </a>
+    </div>
+</header>
