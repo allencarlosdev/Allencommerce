38cf26e071570a4f2809da1a2d223f297155a2b5
CarlosAllen93 <allencarlosdev@gmail.com>
2021-08-26 17:01:27 -0400

    adding menu functionality in AlpineJs

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,19 +1,12 @@
-<style>
-    #navigation-menu {
-        height: calc(100vh - 4rem);
-    }
-
-    .navigation-link:hover .navigation-submenu {
-        display:block !important;
-    }
-</style>
-<header class="bg-Emerald-900 sticky top-0">
+<header class="bg-Emerald-900 sticky top-0"x-data="dropdown()">
     <div class="container flex items-center h-16">
-        <a class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full" href="http://">
-            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-            <span class="">Categories</span>
+        <a
+            x-on:click="show()"
+            class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
+                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+                <span class="">Categories</span>
         </a>
         <a href="/" class="mx-4">
 
@@ -83,9 +76,14 @@
         <!-- dropdown cart end-->
     </div>
 
-    <nav id="navigation-menu" class="bg-Emerald-900 bg-opacity-25 w-full absolute">
+    <nav id="navigation-menu"
+    :class="{'block': open, 'hidden': !open}"
+    x-show="open"
+    class="bg-Emerald-900 bg-opacity-25 w-full absolute hidden">
         <div class="container h-full">
-            <div class="grid grid-cols-4 h-full relative">
+            <div
+            x-on:click.away="close()"
+            class="grid grid-cols-4 h-full relative">
                 <ul class="bg-white">
                     @foreach($categories as $category)
                         <li class="navigation-link text-Emerald-700 hover:bg-Emerald-500 hover:text-white">
