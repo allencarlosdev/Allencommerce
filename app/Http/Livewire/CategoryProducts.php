9e8e7b5131446c81e46350d837ca2f22aa68c490
CarlosAllen93 <allencarlosdev@gmail.com>
2021-08-30 18:54:48 -0400

    adding slider of products (cell phones and tablets)

--- app/Http/Livewire/CategoryProducts.php
+++ app/Http/Livewire/CategoryProducts.php
@@ -6,6 +6,9 @@ use Livewire\Component;
 
 class CategoryProducts extends Component
 {
+    public $category;
+
+
     public function render()
     {
         return view('livewire.category-products');
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,23 @@
 <x-app-layout>
+    <div class="container py-8">
+        <section>
+            <h1 class="text-lg uppercase font-semibold text-Emerald-700">
+                {{ $categories->first()->name }}
+            </h1>
+            @livewire('category-products', ['category' => $categories->first()])
+        </section>
+    </div>
 
+    <script>
+        new Glider(document.querySelector('.glider'), {
+            slidesToScroll: 1,
+            slidesToShow: 5.5,
+            draggable: true,
+            dots: '.dots',
+            arrows: {
+                prev: '.glider-prev',
+                next: '.glider-next'
+            }
+        });
+    </script>
 </x-app-layout>
